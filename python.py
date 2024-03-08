b0bc1767008c98ae6d7b427d270ff56c9d29029f
linder <achielinder@gmail.com>
2024-03-08 15:15:06 +0300

    done

---
+++ python.py
@@ -0,0 +1,15 @@
+# question one 
+
+# question two
+books = ( "atlas of the heart", "white noise", "a little life")
+for book in books:
+    print(book)
+
+# question three
+person = {"Name":"Mercy","Age":"40", "Favourite Color":"White"}
+print(person)
+
+# question four
+
+# question five
+words = ["car","person","food"]
