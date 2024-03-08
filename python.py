88407555ddca6c99ba8765608dbdca603f4a9548
linder <achielinder@gmail.com>
2024-03-08 17:51:31 +0300

    done

--- python.py
+++ python.py
@@ -1,15 +1,36 @@
-# question one 
+# # question one 
 
-# question two
-books = ( "atlas of the heart", "white noise", "a little life")
-for book in books:
-    print(book)
+# # question two
+# books = ( "atlas of the heart", "white noise", "a little life")
+# for book in books:
+#     print(book)
 
-# question three
-person = {"Name":"Mercy","Age":"40", "Favourite Color":"White"}
-print(person)
+# # question three
+# person = {"Name":"Mercy","Age":"40", "Favourite Color":"White"}
+# print(person)
 
-# question four
+# # question four
 
-# question five
-words = ["car","person","food"]
+
+# # question five
+# words = ["car","person","food" "yes"]
+# odd_length_words = [word for word in words if len(word) % 2 != 0]
+# print(odd_length_words)
+
+my_list = []
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+my_list.insert(1, 15)
+print(my_list)
+another_list =[50,60,70]
+my_list.extend(another_list)
+print(my_list)
+my_list.remove(70)
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list.index(30)
+print (my_list.index(30))
